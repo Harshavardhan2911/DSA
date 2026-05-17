596648e137c5baf1088468d105833b810d38ed3e
Harshavardhan2911 <harshavardhandharmapuri5614@gmail.com>
2026-05-17 21:32:11 +0530

    Added rotate_array_by_k_elements.py solution

--- rotate_array_by_k_elements.py
+++ rotate_array_by_k_elements.py
@@ -1,5 +1,6 @@
 def rotate_array(arr,k):
-    return arr[k:]+arr[:k]
+    rotate=k%len(arr)
+    return arr[rotate:]+arr[:rotate]
 val=list(map(int,input().split()))
 k=int(input())
 print(rotate_array(val,k))
